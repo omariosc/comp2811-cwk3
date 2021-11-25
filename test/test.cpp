b999b57c94ad4f58a562e07e701601362b805bda
Rory652 <el20rdc@leeds.ac.uk>
2021-11-25 13:16:29 +0000

    Added test project

---
+++ test/test.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <QApplication>
+#include <QtMultimediaWidgets/QVideoWidget>
+#include <QMediaPlaylist>
+#include <string>
+#include <vector>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QHBoxLayout>
+#include <QtCore/QFileInfo>
+#include <QtWidgets/QFileIconProvider>
+#include <QDesktopServices>
+#include <QImageReader>
+#include <QMessageBox>
+#include <QtCore/QDir>
+#include <QtCore/QDirIterator>
+
+int main(int argc, char *argv[]) {
+
+    // let's just check that Qt is operational first
+    qDebug() << "Qt version: " << QT_VERSION_STR << endl;
+
+    // create the Qt Application
+    QApplication app(argc, argv);
+
+    // wait for the app to terminate
+    return app.exec();
+}
