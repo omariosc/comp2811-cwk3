13788bcb8974ca0d0299258737fa1d644b9e3f3a
WilliamToonUOL <sc20wt@leeds.ac.uk>
2021-12-11 16:43:26 +0000

    added some settings to settings page

--- settingspage.cpp
+++ settingspage.cpp
@@ -4,18 +4,19 @@
 #include "mainPage/title_label.h"
 #include "mainPage/navigation_button.h"
 #include <QVariant>
+#include <QLineEdit>
 
 SettingsPage::SettingsPage(QWidget *parent) : QDialog(parent) {
     //setStyleSheet("background: #000000;");
     setModal(1);
     setWindowFlags(Qt::Window | Qt::FramelessWindowHint);
     if (parent) {
-        resize(parent->width()-40, parent->height()-500);
+        resize(parent->width(), parent->height());
     }
 
 
     TitleLabel* title = new TitleLabel();
-    title->setText("Settings");
+    title->setText("SETTINGS");
 
     NavigationButton *returnButton = new NavigationButton("BACK");
     returnButton->setIcon(QIcon(":/returnIcon"));
@@ -37,12 +38,39 @@ SettingsPage::SettingsPage(QWidget *parent) : QDialog(parent) {
     header->setProperty("type", "menuBackground");
     header->setLayout(headerLayout);
 
+    QLabel *textColor = new QLabel("Text Colour: ");
+    textColor->setAlignment(Qt::AlignRight);
+    QLineEdit * textColorLE = new QLineEdit();
+
+    QLabel *backgroundColor = new QLabel("Background Colour: ");
+    backgroundColor->setAlignment(Qt::AlignRight);
+    QLineEdit * backgroundColorLE = new QLineEdit();
+
+    QLabel *language = new QLabel("Language: ");
+    language->setAlignment(Qt::AlignRight);
+    QLineEdit * languageLE = new QLineEdit();
+
+    QGridLayout* settingsLayout = new QGridLayout();
+    settingsLayout->addWidget(textColor, 0,0);
+    settingsLayout->addWidget(textColorLE, 0,1);
+    settingsLayout->addWidget(backgroundColor, 1,0);
+    settingsLayout->addWidget(backgroundColorLE, 1,1);
+    settingsLayout->addWidget(language, 2,0);
+    settingsLayout->addWidget(languageLE, 2,1);
+
+    QWidget *settings = new QWidget();
+    settings->setProperty("type", "content");
+    settings->setLayout(settingsLayout);
+
+
+
 
 
     QVBoxLayout* layout = new QVBoxLayout();
     layout->setMargin(0);
     layout->setSpacing(0);
     layout->addWidget(header);
+    layout->addWidget(settings);
     layout->addStretch();
     setLayout(layout);
     show();
