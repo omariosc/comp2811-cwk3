f5dec8b9635ca0236cd1a6c81c7978deba567731
Rory Coe <el20rdc@leeds.ac.uk>
2021-12-17 06:56:20 +0000

    Ran clang-format

--- libraryPage/videolibrary.cpp
+++ libraryPage/videolibrary.cpp
@@ -3,37 +3,34 @@
 
 #include <QPushButton>
 
-VideoLibrary::VideoLibrary(std::vector<VideoFile> &vids, Player &player) : QScrollArea()
-{
-    setWidgetResizable(1);
-    videos = vids;
+VideoLibrary::VideoLibrary(std::vector<VideoFile> &vids, Player &player)
+    : QScrollArea() {
+  setWidgetResizable(1);
+  videos = vids;
 
-    QWidget *buttonScrollArea = new QWidget();
-    buttonScrollArea->setProperty("type", "content");
+  QWidget *buttonScrollArea = new QWidget();
+  buttonScrollArea->setProperty("type", "content");
 
-    QGridLayout *layout = new QGridLayout();
-    QSizePolicy buttonScrollAreaSizePolicy =  QSizePolicy();
-    buttonScrollAreaSizePolicy.setHorizontalPolicy(QSizePolicy::Ignored);
-    layout->setMargin(0);
-    layout->setSpacing(0);
-    buttonScrollArea->setLayout(layout);
+  QGridLayout *layout = new QGridLayout();
+  QSizePolicy buttonScrollAreaSizePolicy = QSizePolicy();
+  buttonScrollAreaSizePolicy.setHorizontalPolicy(QSizePolicy::Ignored);
+  layout->setMargin(0);
+  layout->setSpacing(0);
+  buttonScrollArea->setLayout(layout);
 
+  for (int i = 0; i < videos.size(); i++) {
+    ThumbnailButton *button = new ThumbnailButton(buttonScrollArea);
+    button->connect(button, SIGNAL(jumpTo(VideoFile *)), &player,
+                    SLOT(jumpTo(VideoFile *)));
+    buttons.push_back(button);
+    layout->addWidget(button, i / 4, i % 4);
+    button->init(&videos.at(i));
+  }
 
-    for (int i = 0; i < videos.size(); i++) {
-        ThumbnailButton *button = new ThumbnailButton(buttonScrollArea);
-        button->connect(button, SIGNAL(jumpTo(VideoFile*)), &player, SLOT(jumpTo(VideoFile*)));
-        buttons.push_back(button);
-        layout->addWidget(button, i / 4, i % 4);
-        button->init(&videos.at(i));
-    }
-
-
-    setWidget(buttonScrollArea);
-    buttonScrollArea->setSizePolicy(buttonScrollAreaSizePolicy);
-
+  setWidget(buttonScrollArea);
+  buttonScrollArea->setSizePolicy(buttonScrollAreaSizePolicy);
 }
 
-
-std::vector<ThumbnailButton*>* VideoLibrary::getButtons(){
-    return &(buttons);
+std::vector<ThumbnailButton *> *VideoLibrary::getButtons() {
+  return &(buttons);
 }
--- mainPage/navigation_button.h
+++ mainPage/navigation_button.h
@@ -4,25 +4,24 @@
 #include <QToolButton>
 
 class NavigationButton : public QToolButton {
-    Q_OBJECT
+  Q_OBJECT
 
 public:
-    NavigationButton(QString newName = "");
-    void setName(QString newName);
-    void setPageNum(int pageNum);
-    void setActive(bool newState);
+  NavigationButton(QString newName = "");
+  void setName(QString newName);
+  void setPageNum(int pageNum);
+  void setActive(bool newState);
 
 signals:
-    void clicked(int pageNumber, QString pageName);
+  void clicked(int pageNumber, QString pageName);
 
 private slots:
-    void dataClicked();
+  void dataClicked();
 
 private:
-    QString name;
-    int pageNum;
-    bool active;
-
+  QString name;
+  int pageNum;
+  bool active;
 };
 
 #endif // THE_NAVIGATION_BUTTON_H
