403723e85e60b96f7ec06405a498735e301e2411
Rory Coe <el20rdc@leeds.ac.uk>
2021-12-17 07:15:13 +0000

    Ran clang-format

--- libraryPage/videolibrary.cpp
+++ libraryPage/videolibrary.cpp
@@ -1,39 +1,37 @@
 #include "libraryPage/videolibrary.h"
-#include <QGridLayout>
 #include "player.h"
+#include <QGridLayout>
 
 #include <QPushButton>
 
-VideoLibrary::VideoLibrary(std::vector<VideoFile> &vids,Player* player) : QScrollArea(){
-    setWidgetResizable(1);
-    videos = vids;
+VideoLibrary::VideoLibrary(std::vector<VideoFile> &vids, Player *player)
+    : QScrollArea() {
+  setWidgetResizable(1);
+  videos = vids;
 
-    QWidget *buttonScrollArea = new QWidget();
-    buttonScrollArea->setProperty("type", "content");
+  QWidget *buttonScrollArea = new QWidget();
+  buttonScrollArea->setProperty("type", "content");
 
-    QGridLayout *layout = new QGridLayout();
-    QSizePolicy buttonScrollAreaSizePolicy =  QSizePolicy();
-    buttonScrollAreaSizePolicy.setHorizontalPolicy(QSizePolicy::Ignored);
-    layout->setMargin(0);
-    layout->setSpacing(0);
-    buttonScrollArea->setLayout(layout);
+  QGridLayout *layout = new QGridLayout();
+  QSizePolicy buttonScrollAreaSizePolicy = QSizePolicy();
+  buttonScrollAreaSizePolicy.setHorizontalPolicy(QSizePolicy::Ignored);
+  layout->setMargin(0);
+  layout->setSpacing(0);
+  buttonScrollArea->setLayout(layout);
 
+  for (int i = 0; i < videos.size(); i++) {
+    ThumbnailButton *button = new ThumbnailButton(buttonScrollArea);
+    button->connect(button, SIGNAL(jumpTo(VideoFile *)), player,
+                    SLOT(playVideo(VideoFile *)));
+    buttons.push_back(button);
+    layout->addWidget(button, i / 3, i % 3);
+    button->init(&videos.at(i));
+  }
 
-    for (int i = 0; i < videos.size(); i++) {
-        ThumbnailButton *button = new ThumbnailButton(buttonScrollArea);
-        button->connect(button, SIGNAL(jumpTo(VideoFile*)), player, SLOT(playVideo(VideoFile*)));
-        buttons.push_back(button);
-        layout->addWidget(button, i / 3, i % 3);
-        button->init(&videos.at(i));
-    }
-
-
-    setWidget(buttonScrollArea);
-    buttonScrollArea->setSizePolicy(buttonScrollAreaSizePolicy);
-
+  setWidget(buttonScrollArea);
+  buttonScrollArea->setSizePolicy(buttonScrollAreaSizePolicy);
 }
 
-
-std::vector<ThumbnailButton*>* VideoLibrary::getButtons(){
-    return &(buttons);
+std::vector<ThumbnailButton *> *VideoLibrary::getButtons() {
+  return &(buttons);
 }
--- libraryPage/videolibrary.h
+++ libraryPage/videolibrary.h
@@ -1,21 +1,21 @@
 #ifndef VIDEOLIBRARY_H
 #define VIDEOLIBRARY_H
 
-#include <QScrollArea>
-#include "video_file.h"
-#include "thumbnail_button.h"
 #include "player.h"
+#include "thumbnail_button.h"
+#include "video_file.h"
+#include <QScrollArea>
 
 class VideoLibrary : public QScrollArea {
-    Q_OBJECT
+  Q_OBJECT
 
 public:
-    VideoLibrary(std::vector<VideoFile> &vids,Player* player);
-    std::vector<ThumbnailButton*>* getButtons();
+  VideoLibrary(std::vector<VideoFile> &vids, Player *player);
+  std::vector<ThumbnailButton *> *getButtons();
 
 private:
-    std::vector<ThumbnailButton*> buttons;
-    std::vector<VideoFile> videos;
+  std::vector<ThumbnailButton *> buttons;
+  std::vector<VideoFile> videos;
 };
 
 #endif // VIDEOLIBRARY_H
